2d0f88b0b6671d7d29a2ab4aa21baa26c15a6c52
BBorgi <49433727+BBorgi@users.noreply.github.com>
2022-10-07 16:21:03 +0200

    Changed ALLOWED_HOSTS

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
